8edbbd1599de827c996240140db66c1217448a5a
Augusto <villevip10@gmail.com>
2024-06-13 18:51:10 -0300

    Opt 6

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -13,14 +13,22 @@ export const asureAuth = (req, res, next) => {
     try {
      
         const payload = decoded(token);
-        console.log(payload);
+     
+        const { exp } = payload;
+        const currentData = new Date().getTime();
     
+        if( exp <= currentData ) {
+            return res.status(400).send({ msg: 'Token a expirado' })
+        };
+
+        req.user = payload;
+        
+        next();
+
     } catch (error) {
      
         return res.status(400).send(
         { msg: 'token invalido' });  
     };
-   
-
-    next();
+       
 };
