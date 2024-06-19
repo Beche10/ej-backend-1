c852b808a2e029187c75895a990a379ea3ce5da8
Augusto <villevip10@gmail.com>
2024-06-19 19:26:16 -0300

    Casi definitivo

--- controllers/user.js
+++ controllers/user.js
@@ -8,8 +8,6 @@ export const getMe = (req, res) => {
 
     const { user_id } = req.user;
 
-    console.log(req.user);
-
     const user = usersDb.find(user => user.id === user_id);
 
     if (!user) { 
@@ -18,6 +16,7 @@ export const getMe = (req, res) => {
         return res.status(200).send({ user });
     };
 }; 
+
 
 export const getUsers = (req, res) => {
     const { active } = req.query;
@@ -34,13 +33,11 @@ export const getUsers = (req, res) => {
     return res.status(200).send({ response });
 };
 
+
 export const createUser = (req, res) => {
 
     const { firstname, lastname, email, password, role, active, avatar }  = req.body;
     
-    //const salt = bcrypt.genSaltSync(10);
-    //const hashPassword = bcrypt.hashSync(password, salt);
-
     const user = User.createUser(
         firstname,
         lastname,
@@ -50,9 +47,7 @@ export const createUser = (req, res) => {
         false,
         ''        
     );
-    
-    console.log(user);
-    
+            
     if (req.files.avatar) {
         const imagePath = getFilePath(req.files.avatar);
         user.avatar = imagePath;
@@ -60,11 +55,42 @@ export const createUser = (req, res) => {
 
     usersDb.push(user);
     
-
     return res.status(200).send({ msg: user });
-
 };
 
 
+export const updateUser = (req, res) => {
+    const { id } = req.params;
+    const userData = req.body;
 
+    console.log(userData);
 
+    const userIndex = usersDb.findIndex(user => user.id === parseInt(id));
+
+    if (userIndex === -1) {
+        return res.status(400).send({ msg: 'Usuario no encontrado' });
+    };
+
+    const user = usersDb[userIndex];
+    
+    const dataUpdate = ['firstname', 'lastname', 'email', 'role', 'active'];
+    dataUpdate.forEach(data => {
+        if (userData[data] !== undefined) {
+            user[data] = userData[data];
+        }
+    });
+
+    if (userData.password) {
+        const salt = bcrypt.genSaltSync(10);
+        userData.password = bcrypt.hashSync(userData.password, salt);
+    };
+
+    if (req.files.avatar) {
+        const imagePath = getFilePath(req.files.avatar);
+        user.avatar = imagePath;
+    };
+    
+    usersDb[userIndex] = user;
+
+    return res.status(200).send({ msg: 'Actualización correcta', user });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,18 @@
 import multiparty from 'connect-multiparty';
 import { Router } from 'express';
-import { createUser, getMe, getUsers } from '../controllers/user.js';
+import { createUser, getMe, getUsers, updateUser } from '../controllers/user.js';
 import { asureAuth } from '../middlewares/authenticated.js';
 
 
 const mdUpload = multiparty({ uploadDir: './uploads/avatar'});
+
 export const usersRoutes = Router();
 
 
 usersRoutes.get('/me', asureAuth, getMe);
 usersRoutes.get('/all', asureAuth, getUsers);
 usersRoutes.post('/user', asureAuth, mdUpload, createUser);
-
+usersRoutes.patch('/:id', asureAuth, mdUpload, updateUser);
 
 
 export default usersRoutes;
